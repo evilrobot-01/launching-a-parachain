64d2c388da5b505a3727717c420aaf11fcfc4b20
Frank Bell <frank@parity.io>
2023-02-28 21:51:55 +0000

    chore: add cumulus to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,11 @@ cd polkadot || exit
 cargo build --release --bin polkadot
 cd ..
 
+echo Building Cumulus...
+cd cumulus || exit
+cargo build --release --bin polkadot-parachain
+cd ..
+
 echo Building parachain...
 cd substrate-parachain-node || exit
 cargo build --release --bin parachain-template-node
