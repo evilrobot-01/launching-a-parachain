a997d9f69fcd1844278f34fe9485d85211e7dfaa
Frank Bell <frank@parity.io>
2023-03-01 16:07:50 +0000

    chore: add subwasm to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,16 @@ cd cumulus || exit
 cargo build --release --bin polkadot-parachain
 cd ..
 
-echo Building parachain...
+echo Building parachain collator...
 cd substrate-parachain-node || exit
 cargo build --release --bin parachain-template-node
 cd ..
+
+# Install subwasm
+if ! which subwasm &> /dev/null
+then
+echo Installing subwasm...
+cargo install --locked --git https://github.com/chevdor/subwasm --tag v0.16.1 || exit
+echo Use \'cargo uninstall subwasm\' to remove...
+fi
+
